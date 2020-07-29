de341ea451972c7d21c52a280376eae37f6e1182
Ao-zhang <821477765@qq.com>
2020-07-29 09:20:49 +0800

    daoimpl层+mongo

--- Freelancer_backend/src/main/java/com/freelancer/freelancer/daoimpl/UserDaoImpl.java
+++ Freelancer_backend/src/main/java/com/freelancer/freelancer/daoimpl/UserDaoImpl.java
@@ -2,6 +2,9 @@ package com.freelancer.freelancer.daoimpl;
 
 import com.freelancer.freelancer.dao.UserDao;
 import com.freelancer.freelancer.entity.User;
+import com.freelancer.freelancer.entity.UserAvatar;
+import com.freelancer.freelancer.entity.WorkEnclosure;
+import com.freelancer.freelancer.repository.UserAvatarRepository;
 import com.freelancer.freelancer.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -14,6 +17,9 @@ public class UserDaoImpl implements UserDao {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    UserAvatarRepository userAvatarRepository;
+
     @Override
     public User checkUser(String name, String password) {
         return userRepository.checkUser(name, password);
@@ -23,18 +29,38 @@ public class UserDaoImpl implements UserDao {
     public User checkDuplicate(String name) { return userRepository.checkDuplicate(name); }
 
     @Override
-    public void addUser(User newUser) { userRepository.save(newUser); }
+    public void addUser(User newUser) {
+        UserAvatar userAvatar = new UserAvatar(newUser.getU_id(), newUser.getAvatar());
+        userAvatarRepository.save(userAvatar);
+        userRepository.save(newUser);
+    }
 
     @Override
     public User findByName(String name) {
         User user = userRepository.findByName(name);
+        user.setPassword(null);
+        Optional<UserAvatar> userAvatar = userAvatarRepository.findById(user.getU_id());
+        if (userAvatar.isPresent()) {
+            user.setAvatar(userAvatar.get().getAvator());
+        }
+        else {
+            user.setAvatar(null);
+        }
         return user;
     }
 
     @Override
     public User findById(Integer uId) {
-        User user = userRepository.getOne(uId);
+        User user = userRepository.getAUser(uId);
         user.setPassword(null);
-        return user; }
+        Optional<UserAvatar> userAvatar = userAvatarRepository.findById(user.getU_id());
+        if (userAvatar.isPresent()) {
+            user.setAvatar(userAvatar.get().getAvator());
+        }
+        else {
+            user.setAvatar(null);
+        }
+        return user;
+    }
 
 }
